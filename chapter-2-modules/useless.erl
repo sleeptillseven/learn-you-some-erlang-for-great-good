b96c2912dffbf982462811e2207b2b3eca43949b
Christoph Jasinski <christoph.jasinski@googlemail.com>
2014-01-13 06:57:26 +0100

    Add function which prints module information

--- chapter-2-modules/useless.erl
+++ chapter-2-modules/useless.erl
@@ -1,5 +1,6 @@
 -module(useless).
--export([add/2, hello/0, greet_and_add_two/1]).
+-export([add/2, hello/0, greet_and_add_two/1, print_module_info/0]).
+-define(HOUR, 3600).
 
 add(A, B) ->
     A+B.
@@ -12,3 +13,6 @@ hello() ->
 greet_and_add_two(X) ->
     hello(),
     add(X, 2).
+
+print_module_info() ->
+  module_info().
