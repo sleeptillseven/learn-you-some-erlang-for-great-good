fffab6af511ee9a41a84822da3ebdba916938bc1
Christoph Jasinski <christoph.jasinski@googlemail.com>
2014-01-15 18:18:06 +0100

    Rewrite beach function, but use function heads instead

--- chapter-3-syntax-in-functions/cases.erl
+++ chapter-3-syntax-in-functions/cases.erl
@@ -1,5 +1,5 @@
 -module(cases).
--export([insert/2, beach/1]).
+-export([insert/2, beach/1, beachf/1]).
 
 %% With the case-of construct you can make use of
 %% pattern matching on each argument.
@@ -23,3 +23,13 @@ beach(Temperature) ->
         _ ->
             'avoid the beach'
     end.
+
+%% A rewrite of beach but using function heads
+beachf({celsius, N}) when N >= 20, N =< 45 ->
+    'favorable';
+beachf({kelvin, N}) when N >= 293, N =< 318 ->
+    'scientifically favorable';
+beachf({fahrenheit, N}) when N >= 68, N =< 113 ->
+    'favorable in the US';
+beachf(_) ->
+    'avoid beach'.
