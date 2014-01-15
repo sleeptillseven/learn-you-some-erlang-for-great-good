78a25e3447b2dee07c17101919a4ce37edd38eae
Christoph Jasinski <christoph.jasinski@googlemail.com>
2014-01-15 17:49:05 +0100

    Add function with else branch inside if clause

--- chapter-3-syntax-in-functions/what_the_if.erl
+++ chapter-3-syntax-in-functions/what_the_if.erl
@@ -1,12 +1,12 @@
 -module(what_the_if).
--export([heh_if/0]).
+-export([heh_fine/0, oh_my/1]).
 
 %% This method demonstrates the need of a return value
 %% of functions and that an true branch is needed.
 %% Due to the fact that all if clauses are concatenated
 %% by AND and (the last) one clause raises an issue,
 %% the whole functions breaks.
-heh_if() ->
+heh_fine() ->
     if 1 =:= 1 ->
          works % is a true branch
     end,
@@ -16,3 +16,10 @@ heh_if() ->
     if 1 =:= 2, 1 =:= 1 ->
          fails % is a false branch and there will never succeed
     end.
+
+%% This function will never break cause it has an
+%% 'else' clause.
+oh_my(N) ->
+    if N =:= 2 -> might_succeed;
+    true -> always_does %% This is Erlang's if's 'else!'
+end.
