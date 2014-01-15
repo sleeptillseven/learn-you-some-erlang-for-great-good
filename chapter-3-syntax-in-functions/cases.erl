df62d031022fce04237a1d33f31c1af812bd8ac4
Christoph Jasinski <christoph.jasinski@googlemail.com>
2014-01-15 18:14:34 +0100

    A more complex example of case of

--- chapter-3-syntax-in-functions/cases.erl
+++ chapter-3-syntax-in-functions/cases.erl
@@ -1,5 +1,5 @@
 -module(cases).
--export([insert/2]).
+-export([insert/2, beach/1]).
 
 %% With the case-of construct you can make use of
 %% pattern matching on each argument.
@@ -9,4 +9,17 @@ insert(X, Set) ->
     case lists:member(X, Set) of
         true  -> Set;
         false -> [X|Set]
+    end.
+
+%% checking more complex input via case-of
+beach(Temperature) ->
+    case Temperature of
+        {celsius, N} when N >= 20, N =< 45 ->
+            'favorable';
+        {kelvin, N} when N >= 293, N =< 318 ->
+            'scientifically favorable';
+        {fahrenheit, N} when N >= 68, N =< 113 ->
+            'favorable in the US';
+        _ ->
+            'avoid the beach'
     end.
